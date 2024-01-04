68b6ef0fcc0ad020972b4e100d43f4e00ba339b7
Akash <akashj.vasava@gmail.com>
2024-01-04 20:29:03 +0530

    Added Glass Effect to the different sections of the page

--- src/Components/User.js
+++ src/Components/User.js
@@ -4,13 +4,20 @@ export default function User() {
   const imgAdd =
     "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
 
+  const banner = "https://static.vecteezy.com/system/resources/thumbnails/001/234/358/small/modern-blue-halftone-banner-background.jpg";
+
   return (
-    <>
+    <div className="flex justify-center items-center flex-col w-full bg-black">
     {/* User Photo */}
-      <div className="flex justify-center mt-4">
-        <div className="container w-3/5 h-[200px] border-2 border-black rounded-sm flex justify-center items-center">
+      <div className="flex justify-center mt-4 w-3/5 relative">
+        <div className="container h-[200px] border-2 border-black rounded-sm flex justify-center items-center backdrop-blur-3xl bg-white/30 overflow-hidden">
+        <img
+            className="w-full h-full object-cover"
+            src={banner}
+            alt="Banner"
+          />
           <img
-            className="rounded-full h-[150px] z-10"
+            className="rounded-full absolute z-10 top-1/2 -translate-y-1/2 left-1/2 transform -translate-x-1/2 h-[150px]"
             src={imgAdd}
             alt="UserPhoto"
           />
@@ -19,29 +26,32 @@ export default function User() {
       {/* User Photo */}
 
       {/* User Details */}
-      <div className="flex flex-col items-center mt-4">
-        <div className="container w-3/5 border-2 border-black border-b-0 rounded-sm text-left">
-          <div className="font-bold text-xl bg-slate-400 w-full p-1">
+      <div className="flex flex-col items-center mt-4 backdrop-blur-xl bg-white/60 w-3/5 ">
+        <div className="container border border-black border-b-0 rounded-sm text-left backdrop-blur-xl bg-white/30">
+          <div className="font-bold text-xl w-full p-1">
             <span className="ml-2">User Details</span>
           </div>
         </div>
-        <div className="container w-3/5 border-2 border-black border-b-0 rounded-sm text-left">
-          <div className="font-bold text-xl bg-slate-200 w-full p-1">
+        <div className="container border border-black border-b-0 rounded-sm text-left">
+          <div className="font-semibold text-xl w-full p-1">
             <span className="ml-2">User-Name: Akash</span>
           </div>
         </div>
-        <div className="container w-3/5 border-2 border-black border-b-0 rounded-sm text-left">
-          <div className="font-bold text-xl bg-slate-200 w-full p-1">
+        <div className="container border border-black border-b-0 rounded-sm text-left">
+          <div className="font-semibold text-xl w-full p-1">
             <span className="ml-2">Email: akash@gmail.com</span>
           </div>
         </div>
-        <div className="container w-3/5 border-2 border-black border-b-0  rounded-sm text-left">
-          <div className="font-bold text-xl bg-slate-200 w-full p-1">
+        <div className="container border border-black border-b-0  rounded-sm text-left">
+          <div className="font-semibold text-xl w-full p-1">
             <span className="ml-2">Password: ******</span>
           </div>
         </div>
-        <div className="container w-3/5 border-2 border-black rounded-sm text-left">
-          <button className="font-bold text-xl bg-slate-700 w-full p-1 text-center">
+        <div className="container border border-black rounded-sm text-left flex">
+        <div className="edit icon w-1/10 text-left">
+        {/* Add Edit Icon*/}
+        </div>
+          <button className="font-bold text-xl w-8/10 p-1 text-left">
             Update Details
           </button>
         </div>
@@ -49,24 +59,25 @@ export default function User() {
       {/* User Details */}
 
       {/* Playlist/Song Modification */}
-      <div className="flex flex-col items-center my-4">
-        <div className="container w-3/5 border-2 border-black border-b-0 rounded-sm text-left">
-          <div className="font-bold text-xl bg-slate-400 w-full p-1">
+      
+      <div className="flex flex-col items-center mt-4 backdrop-blur-xl bg-white/60 w-3/5 mb-4">
+        <div className="container border border-black border-b-0 rounded-sm text-left backdrop-blur-xl bg-white/30">
+          <div className="font-bold text-xl w-full p-1">
             <span className="ml-2">Modify Playlist / Song</span>
           </div>
         </div>
-        <div className="container w-3/5 border-2 border-black border-b-0 rounded-sm text-left">
-          <div className="font-bold text-xl bg-slate-200 w-full p-1">
+        <div className="container border border-black border-b-0 rounded-sm text-left">
+          <div className="font-semibold text-xl w-full p-1">
             <span className="ml-2">Modify Playlist</span>
           </div>
         </div>
-        <div className="container w-3/5 border-2 border-black rounded-sm text-left">
-          <div className="font-bold text-xl bg-slate-200 w-full p-1">
+        <div className="container border border-black rounded-sm text-left">
+          <div className="font-semibold text-xl w-full p-1">
             <span className="ml-2">Modify Song</span>
           </div>
         </div>
       </div>
       {/* Playlist/Song Modification */}
-    </>
+    </div>
   );
 }
